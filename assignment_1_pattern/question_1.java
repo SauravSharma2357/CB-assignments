15c0d3e627ed0d63cbc92c297a81176d89d553db
King Celestial <sauravsharma2357@gmail.com>
2023-07-27 20:19:21 +0530

    pattern

--- assignment_1_pattern/question_1.java
+++ assignment_1_pattern/question_1.java
@@ -1,9 +1,14 @@
 package assignment_1_pattern;
 
+import java.util.Scanner;
+
 public class question_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		 
 
 	}
 
