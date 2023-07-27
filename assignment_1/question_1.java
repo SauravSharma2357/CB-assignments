faed43db9f43f3f7fd69e5f53a0f921437fb5a6b
King Celestial <sauravsharma2357@gmail.com>
2023-07-27 18:44:18 +0530

    pattern

---
+++ assignment_1/question_1.java
@@ -0,0 +1,10 @@
+package assignment_1;
+
+public class question_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CB_assignment {
+}
