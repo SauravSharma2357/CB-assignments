2cf2ed1e8c903df5b098ceb27d84b3daf8d528b3
King Celestial <sauravsharma2357@gmail.com>
2023-07-29 09:18:38 +0530

    pattern

---
+++ assignment_1_pattern/question_13_pascal_triangle.java
@@ -0,0 +1,28 @@
+package assignment_1_pattern;
+
+import java.util.Scanner;
+
+public class question_13_pascal_triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int star =1;
+		int row=0;
+		while(row<n) {
+			int i=0;
+			int val = 1;  //This
+			while(i<star) {
+				System.out.print(val + " ");
+				val = ((row-i)*val)/(i+1);
+				i++;
+			}
+			System.out.println();
+			row++;
+			star++;
+		}
+
+	}
+
+}
