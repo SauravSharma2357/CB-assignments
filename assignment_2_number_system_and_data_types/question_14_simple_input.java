9de6b242c1b8edd1087438b23b8bf9d0633489c7
King Celestial <sauravsharma2357@gmail.com>
2023-08-03 22:41:45 +0530

    number_system_and_data_types

--- assignment_2_number_system_and_data_types/question_14_simple_input.java
+++ assignment_2_number_system_and_data_types/question_14_simple_input.java
@@ -7,14 +7,14 @@ public class question_14_simple_input {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kc =new Scanner(System.in);
-		int i =1;
-		while(i>0) {
-			int n = kc.nextInt();
-			int j=n;
-			while(i>0) {
-				j=j+n;
-			}
+		long i =0;
+		while(i>=0) {
+			long n = kc.nextLong();
 			i=i+n;
+			if(i<0) {
+				break;
+			}
+			System.out.println(n);
 		}
 		
 		
---
+++ assignment_2_number_system_and_data_types/question_19_odd_and_even_back_in_delhi.java
@@ -0,0 +1,36 @@
+package assignment_2_number_system_and_data_types;
+
+import java.util.Scanner;
+
+public class question_19_odd_and_even_back_in_delhi {
+
+	public static void main(String[] args) {
+		 //TODO Auto-generated method stub
+		Scanner kc = new Scanner(System.in); 
+		long N = kc.nextLong();
+		for(int i=1;i<=N;i++) {
+			long T = kc.nextLong();
+			long rem = 0;
+			long even = 0;
+			long odd = 0;
+			while(T!=0) {
+				rem=T%10;
+				if(rem%2==0) {
+					even=even+rem;
+				}
+				else {
+					odd=odd+rem;
+				}
+				T=T/10;
+			}
+			if(even%4==0 || odd%3==0) {
+				System.out.println("Yes");
+			}
+			else {
+				System.out.println("No");
+			}
+		}
+
+	}
+
+}
