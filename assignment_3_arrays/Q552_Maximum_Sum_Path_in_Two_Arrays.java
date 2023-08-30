6baefa1b8b55a1716e4fd083e052a74d532a88a8
King Celestial <sauravsharma2357@gmail.com>
2023-08-30 16:23:28 +0530

    Maximum sum path array solved finally

--- assignment_3_arrays/Q552_Maximum_Sum_Path_in_Two_Arrays.java
+++ assignment_3_arrays/Q552_Maximum_Sum_Path_in_Two_Arrays.java
@@ -7,8 +7,8 @@ public class Q552_Maximum_Sum_Path_in_Two_Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kc = new Scanner(System.in);
-		int T = kc.nextInt();
-		for (int i = 1; i <= T; i++) {
+		int t = kc.nextInt();
+		for (int i = 1; i <= t; i++) {
 			int n = kc.nextInt();
 			int m = kc.nextInt();
 			int[] arr1 = new int[n];
@@ -19,31 +19,40 @@ public class Q552_Maximum_Sum_Path_in_Two_Arrays {
 			for (int k = 0; k < arr2.length; k++) {
 				arr2[k] = kc.nextInt();
 			}
-			System.out.println(maxPathSum(arr1, arr2,m,n));
+			System.out.println(maxPathSum(arr1, arr2, n, m));
 		}
 	}
 
-	    public static int maxPathSum(int ar1[], int ar2[], int m, int n) {
-	        int i = 0, j = 0;
-	        int result = 0, sum1 = 0, sum2 = 0;
-	        while (i < m && j < n) {
-	            if (ar1[i] < ar2[j])
-	                sum1 += ar1[i++];
-	            else if (ar1[i] > ar2[j])
-	                sum2 += ar2[j++];
-	            else {
-	                result += Math.max(sum1, sum2) + ar1[i];
-	                sum1 = 0;
-	                sum2 = 0;
-	                i++;
-	                j++;
-	            }
-	        }
-	        while (i < m)
-	            sum1 += ar1[i++];
-	        while (j < n)
-	            sum2 += ar2[j++];
-	        result += Math.max(sum1, sum2);
-	        return result;
-	    }
+	public static int maxPathSum(int[] arr1, int[] arr2, int n, int m) {
+		int i = 0;
+		int j = 0;
+		int result = 0;
+		int sum1 = 0;
+		int sum2 = 0;
+		while (i < n && j < m) {
+			if (arr1[i] < arr2[j]) {
+				sum1 = sum1 + arr1[i];
+				i++;
+			} else if (arr1[i] > arr2[j]) {
+				sum2 += arr2[j];
+				j++;
+			} else {
+				result = result + Math.max(sum1, sum2) + arr1[i];
+				sum1 = 0;
+				sum2 = 0;
+				i++;
+				j++;
+			}
+		}
+		while (i < n) {
+			sum1 = sum1 + arr1[i];
+			i++;
+		}
+		while (j < m) {
+			sum2 = sum2 + arr2[j];
+			j++;
+		}
+		result = result + Math.max(sum1, sum2);
+		return result;
 	}
+}
--- assignment_3_arrays/test.java
+++ assignment_3_arrays/test.java
@@ -1,67 +1,24 @@
 package assignment_3_arrays;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int loop = sc.nextInt();
-		
-		while(loop >= 1){
-			int size1 = sc.nextInt();
-			int size2 = sc.nextInt();
-			int [] a = new int[size1];
-			int [] b = new int[size2];
-			
-			for(int i=0; i<a.length; i++) {
-				a[i]= sc.nextInt(); 
-			}
-			
-			for(int i=0; i<b.length; i++) {
-				b[i]= sc.nextInt(); 
-			}
-			System.out.print(maxPathSum(a,b));
-			loop--;
-		}
-	}
-	
-	public static int maxPathSum(int[] a, int[] b) {
-		int sum1 = 0, sum2 = 0, result = 0;
-		int sizea = a.length, sizeb = b.length;
-		int i = 0, j = 0;
-		
-		while(i < sizea && j < sizeb) {
-			if(a[i] > b[j]) {
-				sum2 += b[j];//System.out.println("Sum2 "+sum2);
-				j++; //System.out.println("j "+j);
-			}
-			else if(b[j] > a[i]) {
-				sum1 += a[i];//System.out.println("Sum1 "+sum1);
-				i++; //System.out.println("i "+i);
-			}
-			else {
-				result += Math.max(sum1, sum2) + a[i];
-				//System.out.println("result "+result);
-				sum1 = 0;
-				sum2 = 0;
-				i++;j++;
-			}
-		}
-		
-		while(i < sizea) {
-			sum1 += a[i];
-			i++;
-		}
-		
-		while(j < sizeb) {
-			sum2 += b[j];
-			j++;
-		}
-		
-		result += Math.max(sum1, sum2);
-				
-		return result;
-	}
+    public static void main(String args[]) {
+        Scanner kc = new Scanner(System.in);
+        int N = kc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i<arr.length;i++){
+            arr[i] = kc.nextInt();
+        }
+        Inverse(arr,N);
+        
+    }
+    public static void Inverse(int[] arr, int N){
+        int[] arr1 = new int[N];
+        for(int i=0;i<arr1.length;i++){
+            arr1[arr[i]]=i;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
